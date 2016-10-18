d29dc23331923e737cfbed5b6930b2ef88987309
Phil Seidel <psbyron3@gmail.com>
2016-10-18 12:31:42 -0700

    [env] add webpack.config.prod.

---
+++ webpack.config.prod.js
@@ -0,0 +1,45 @@
+  module.exports = {
+
+  entry: [
+    './client/index.jsx'
+  ],
+  output: {
+    path: __dirname + '/client/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+    {
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      }
+    },
+    // {
+    //   test: /\.scss$/,
+    //   loaders: ["style", "css", "sass"]
+    // }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  },
+    plugins:[
+      new webpack.DefinePlugin({
+        'process.env':{
+          'NODE_ENV': JSON.stringify('production')
+        }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress:{
+          warnings: true
+        }
+      })
+    ]
+  }
