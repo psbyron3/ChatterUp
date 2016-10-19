502ac2b3dd30afac6c2bb87deb735e4f2f88f7ab
Phil Seidel <psbyron3@gmail.com>
2016-10-19 11:44:39 -0700

    [env] Added CORS.

--- server/server.js
+++ server/server.js
@@ -50,13 +50,13 @@ console.log(moment().format('h:mm:ss a') + ": Server is tuning in live on 3000")
 
 /////////////////////CORS////////////////////////////////
 
-// app.use(function(req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 
-//     'Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name');
-//   next();
-// });
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 
+    'Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5,  Date, X-Api-Version, X-File-Name');
+  next();
+});
 
 //////////////////////////////////////////////////////
 /////////// SOCKETS /////////////////////////////////
